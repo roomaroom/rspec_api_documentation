9863b7c10539c3434c183d5a4bd4b6f637e2d967
Vasilij Melnychuk <isqrel@gmail.com>
2014-04-14 12:42:18 +0300

    rad for swagger

--- lib/rspec_api_documentation/writers/json_swagger_writer.rb
+++ lib/rspec_api_documentation/writers/json_swagger_writer.rb
@@ -6,7 +6,10 @@ module RspecApiDocumentation
       delegate :docs_dir, :to => :configuration
 
       def write
-        File.open(docs_dir.join("index.json"), "w+") do |f|
+        FileUtils.rm_rf docs_dir.join('v1')
+        FileUtils.mkdir_p(docs_dir.join('v1'))
+
+        File.open(docs_dir.join('v1', "index.json"), "w+") do |f|
           f.write Formatter.to_json(JsonSwaggerIndex.new(index, configuration))
         end
         write_examples
@@ -14,23 +17,39 @@ module RspecApiDocumentation
 
       def write_examples
         json_examples = {}
+        a = []
         index.examples.each do |example|
-          json_example = JsonSwaggerExample.new(example, configuration)
-          (json_examples[json_example.dirname] ||= []) << JsonSwaggerExample.new(example, configuration)
+          unless example.mute
+            a << example.route
+            json_example = JsonSwaggerExample.new(example, configuration)
+            dirname = example.route.split('/').reject(&:blank?)
+            FileUtils.mkdir_p(docs_dir.join(dirname[0..-2].join('/')))
+            #(json_examples[docs_dir.join(dirname.join('/') << '.json')] ||= []) << JsonSwaggerExample.new(example, configuration)
+            (json_examples[docs_dir.join('v1', (example.root_path.gsub(/^\//, "") << '.json'))] ||= []) << JsonSwaggerExample.new(example, configuration)
+          end
         end
 
         json_examples.each do |dirname, examples|
-          File.open(docs_dir.join(dirname + ".json"), "a+") do |f|
+          #directory = File.dirname(dirname)
+
+          File.open(docs_dir.join(dirname), "a+") do |f|
             f.write Formatter.to_json(format_examples(examples))
           end
         end
       end
 
       def format_examples(json_examples)
+        json_examples_sorted = {}
+        json_examples.each do |example|
+          root_path = example.root_path
+          root_path = example.modified_root_path if example.try(:modified_root_path)
+          (json_examples_sorted[root_path] ||= []) << example
+        end
+
         {
           basePath: @configuration.base_api_path,
           apiVersion: @configuration.api_version,
-          apis: []
+          apis: json_examples_sorted.map { |path, examples| { path: path, operations: examples } }
         }
       end
     end
@@ -51,7 +70,6 @@ module RspecApiDocumentation
 
       def as_json(opts = nil)
         {
-          basePath: @configuration.base_api_path,
           apiVersion: @configuration.api_version,
           apis: section_hash
         }
@@ -93,13 +111,12 @@ module RspecApiDocumentation
 
       def as_json(opts = nil)
         {
-          method: method,
-          operations: [],
-          resource: resource_name,
-          :http_method => http_method,
-          :description => description,
-          :explanation => explanation,
-          :parameters => respond_to?(:parameters) ? parameters : [],
+          method:     http_method,
+          type:       resource_name,
+          summary:    description,
+          nickname:   "#{http_method}#{resource_name}",
+          parameters: respond_to?(:parameters) ? formatted_parameters : [],
+          responseMessages: []
         }
       end
 
@@ -114,6 +131,10 @@ module RspecApiDocumentation
           end
           hash
         end
+      end
+
+      def formatted_parameters
+        parameters
       end
     end
   end
